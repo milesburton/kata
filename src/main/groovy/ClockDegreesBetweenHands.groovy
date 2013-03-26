e4c604b4cad12c17aa20eab6bbd302c47dca6097
miles <miles.burton@agileview.co.uk>
2013-03-26 08:42:57 +0000

    @Miles cleaned up ClockDegreesbetweenHands

--- src/main/groovy/ClockDegreesBetweenHands.groovy
+++ src/main/groovy/ClockDegreesBetweenHands.groovy
@@ -1,15 +1,31 @@
 class ClockDegreesBetweenHands {
 
+    private static final double DEGREES_IN_CIRCLE = 360.0
 
-    double degreesBetween(int hourHand, int secondHand) {
 
-        int secondHandInDegrees = (360 / 60) * secondHand
-        int hoursHandInDegrees = (360 / 12) * hourHand
+    BigDecimal degreesBetween(int hourHand, int secondHand) {
 
-        if (hoursHandInDegrees > secondHandInDegrees) {
-            hoursHandInDegrees - secondHandInDegrees % 360
-        } else {
-            secondHandInDegrees - hoursHandInDegrees % 360
-        }
+        double secondHandInDegrees = calculateDegreesGivenSeconds(secondHand)
+        double hoursHandInDegrees = calculateDegreesGivenHours(hourHand)
+
+        double degreesBetween = calculateDegreesBetween(secondHandInDegrees, hoursHandInDegrees)
+
+        degreesBetween % DEGREES_IN_CIRCLE ? degreesBetween : 0
+    }
+
+    private double calculateDegreesBetween(double secondHandInDegrees, double hoursHandInDegrees) {
+
+        double pointSmallest = Math.min(secondHandInDegrees, hoursHandInDegrees)
+        double pointLargest = Math.max(secondHandInDegrees, hoursHandInDegrees)
+
+        pointLargest - pointSmallest
+    }
+
+    private double calculateDegreesGivenHours(int hourHand) {
+        (DEGREES_IN_CIRCLE / 12) * hourHand
+    }
+
+    private double calculateDegreesGivenSeconds(int secondHand) {
+        (DEGREES_IN_CIRCLE / 60) * secondHand
     }
 }
